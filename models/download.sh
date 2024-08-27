74501256060a41444565bbf980ee0d702348d489
Matthew Foran <matthewjforan@gmail.com>
2024-08-27 15:06:29 -0400

    cleanup

--- models/download.sh
+++ models/download.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
 DIR=$(dirname "$0")
-echo $DIR
 wget -O $DIR/vitpose-l-coco.onnx https://huggingface.co/JunkyByte/easy_ViTPose/resolve/main/onnx/coco/vitpose-l-coco.onnx
 wget -O $DIR/yolov8l.pt https://huggingface.co/Ultralytics/YOLOv8/resolve/main/yolov8l.pt
